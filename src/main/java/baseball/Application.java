423c61932cd7276c17854188822e1a89f94784fa
breezeunn <je7689@gmail.com>
2022-04-18 05:00:31 +0900

    feat(Game): Game 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.domain.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean isQuit;
+
+        do {
+            Game game = new Game();
+            game.run();
+            isQuit = game.isQuit();
+        } while (!isQuit);
     }
 }
---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import baseball.ui.GameInput;
+import baseball.ui.GameOutput;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+    private final Computer computer;
+    public Game() {
+        this.computer = new Computer();
+    }
+    public void run() {
+        boolean isAnswer;
+        do {
+            String userInput = GameInput.getUserNumber();
+            Player player = new Player(userInput);
+
+            Score score = new Score(computer, player);
+            isAnswer = score.isAnswer();
+            System.out.println(GameOutput.getResultMessage(score.getStrikeCount(), score.getBallCount()));
+
+        } while (!isAnswer);
+    }
+    public boolean isQuit() {
+        String cmd = GameInput.getUserCmd();
+        String END_COMMAND = "2";
+        List<String> validCmd = Arrays.asList("1", "2");
+        if (!validCmd.contains(cmd)) {
+            throw new IllegalArgumentException(String.format("Invalid command : %s", cmd));
+        }
+        return END_COMMAND.equals(cmd);
+    }
+}
